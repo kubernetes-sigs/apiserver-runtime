be44ce300237cc26a9c4ed817efe68cfa5441f3f
Phillip Wittrock <povar@pwittrock.me>
2020-09-18 10:15:06 -0700

    Improve docs

--- pkg/builder/doc.go
+++ pkg/builder/doc.go
@@ -15,4 +15,47 @@ limitations under the License.
 */
 
 // Package builder contains a builder for creating a new Kubernetes apiserver.
+//
+// API Extension Servers
+//
+// API extension servers and apiserver aggregation are techniques for extending the Kubernetes API surface
+// without using CRDs.  Rather than registering a resource type as a CRD stored by the apiserver in etcd, apiserver
+// aggregation registers REST endpoints provided by the extension server, and requests are proxied by the main
+// control-plane apiserver to the extension apiserver.
+//
+// Use Cases
+//
+// Following are use cases where one may consider using an extension API server rather than CRDs for implementing
+// an extension resource type.
+//
+// * Resource types which are not backed by storage -- e.g. metrics
+//
+// * Resource types which may not fit in etcd
+//
+// * Using a separate etcd instance for the extension types
+//
+// Registering Types
+//
+// New resource types may be registered with the API server by implementing the go struct for the type under
+// YOUR_MODULE/pkg/apis/YOUR_GROUP/VERSION/types.go and then calling WithResource.
+// You will need to generate deepcopy and openapi go code for your types to be registered.
+//
+// Install the code generators (from your module):
+//
+//    $ go get sigs.k8s.io/tools/apiserver-runtime-gen
+//    $ apiserver-runtime-gen --install
+//
+// Add the code generation tag to you main package:
+//
+//    //go:generate apiserver-runtime-gen
+//    package main
+//
+// Run the code generation after having defined your types:
+//
+//    $ go generate ./...
+//
+// To also generate clients, provide the -g option to apiserver-runtime-gen for the client, lister and informer
+// generators.
+//
+//    $ apiserver-runtime-gen -g client-gen -g deepcopy-gen -g informer-gen -g lister-gen -g openapi-gen
 package builder
